d5cc35966d05407d1cc8898fda3540b7b9e4ea15
Dominik Zeiger <dominik.zeiger@gmail.com>
2024-11-23 17:25:21 +0100

    updates

---
+++ update.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json
+cp latest.json /home/dzeiger/dev/projects/pokemongo-json-pokedex/pokemongo-game-master/versions/latest/V2_GAME_MASTER.json
+npm start
