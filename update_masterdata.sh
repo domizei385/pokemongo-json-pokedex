a9384525cb9e285ceb0841246cf8257ca2d34d84
Dominik Zeiger <dominik.zeiger@gmail.com>
2025-10-22 09:26:44 +0200

    updates

---
+++ update_masterdata.sh
@@ -0,0 +1,5 @@
+curl -ki https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json -o /home/dzeiger/dev/projects/pokemongo-json-pokedex/pokemongo-game-master/versions/latest/V2_GAME_MASTER.json
+npm start
+git add .
+git commit -am "updates"
+git push
